d9f95faca99e800423f44a4190e082e2d58e7894
mohammed1916 <arrahmaan9901@gmail.com>
2025-11-17 14:07:42 +0530

    InvariantCulture normalizes number formatting so your CSV parser sees proper commas between columns, and numbers don’t get merged.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,7 +7,7 @@ using Unity.Barracuda;
 using Unity.MLAgents;
 using UnityEngine;
 using UnityEngine.UIElements;
-
+using System.Globalization;
 public class GameController : MonoBehaviour
 {
     [SerializeField] private int episodeSteps = 1000;
@@ -155,7 +155,9 @@ public class GameController : MonoBehaviour
     
     private void LogBlock(int id, Vector3 p, float locked, float held)
     {
-        blockWriter.WriteLine($"{Time.time:F4},Block{id},{p.x:F4},{p.y:F4},{p.z:F4},{locked},{held}");
+        blockWriter.WriteLine(string.Format(CultureInfo.InvariantCulture,
+        "{0:F4},Block{1},{2:F4},{3:F4},{4:F4},{5},{6}",
+        Time.time, id, p.x, p.y, p.z, locked, held));
     }
 
     private void LogAgent(string agentId, Vector3 p, float active)
@@ -184,7 +186,9 @@ public class GameController : MonoBehaviour
         try
         {
             agentWriters[agentId].WriteLine(
-                $"{Time.time:F4},{p.x:F4},{p.y:F4},{p.z:F4},{active}");
+                string.Format(CultureInfo.InvariantCulture,
+                "{0:F4},{1:F4},{2:F4},{3:F4},{4}",
+                Time.time, p.x, p.y, p.z, active));
         }
         catch (Exception e)
         {
